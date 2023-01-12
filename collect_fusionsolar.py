c28f1c410dd67b8a17a89ee8e456b0f0b1a74de2
Guillaume Blanc <guillaumeblanc.sc@gmail.com>
2023-01-12 13:49:52 +0100

    Uses fusionsolar client directly.

--- collect_fusionsolar.py
+++ collect_fusionsolar.py
@@ -8,13 +8,12 @@ from extern.FusionSolar import fusionsolar
 user = os.environ.get('FUSIONSOLAR_USER', 'unkown')
 password = os.environ.get('FUSIONSOLAR_PASSWORD', 'unkown')
 
-date = pd.Timestamp('20200402', tz='Europe/Brussels')
-
 try:
-    with fusionsolar.PandasClient(user_name=user, system_code=password) as client:
+    with fusionsolar.Client(user_name=user, system_code=password) as client:
         sl = client.get_station_list()
         station_code = sl['data'][0]['stationCode']
 
+        date = pd.Timestamp('20200402', tz='Europe/Brussels')
         df = client.get_kpi_day(station_code=station_code, date=date)
         print(df)
 except Exception as e:
